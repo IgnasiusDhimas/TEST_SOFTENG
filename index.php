0deb36dd46c17f6221dc51054157aed703b47c9e
IgnasiusDhimas <ignatiusdhimasutomo@gmail.com>
2025-09-25 10:57:35 +0700

    nambah file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+od="POST">
+<label>Username:</label>
+<input type="text" name="user" required>
+<br>
+<label>Password:</label>
+<input type="password" name="pass" required>
+<br>
+<button type="submit">Login</button>
+</form>
+<hr>
+<div id="anggota">
+<p>Daftar kontribusi anggota:</p>
+</div>
+</body>
+</html>
